ccd106548702d964c0e22dc41a6866055c893724
AJOH1 <michaelekeh70@gmail.com>
2025-08-17 02:30:03 +0100

    edited2

---
+++ my-movie-app/app/components/TrendingMovies.tsx
@@ -0,0 +1,22 @@
+// components/TrendingMovies.jsx
+import { fetchTrendingMovies } from "@/lib/fetchTrendingMovies";
+import MovieCard from "./MovieCard";
+
+
+type TrendingMoviesProps = {
+  title: string;
+  movies: any[]; // later replace any[] with a Movie type
+};
+
+export default function TrendingMovies({ title, movies }: TrendingMoviesProps) {
+  return (
+    <section>
+      <h2 className="text-xl font-bold mb-4">{title}</h2>
+      <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-5 gap-4">
+        {movies.map((movie) => (
+          <MovieCard key={movie.id} movie={movie} />
+        ))}
+      </div>
+    </section>
+  );
+}
